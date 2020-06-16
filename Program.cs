191b44b29cd6ccbd5d316d50122e8e52a2925d2c
DharoOficial <erickbandeiradharo@gmail.com>
2020-06-16 13:04:27 -0300

    projeto em andamento]

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aula18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Jogador cr7 = new Jogador();
+        cr7.Nome = "Cristiano Ronaldo";
+        System.Console.WriteLine("digite sua funçao em campo, com a primeira letra maiuscula");
+        cr7.Posicao = Console.ReadLine();
+        
+        cr7.Nascimento = DateTime.Parse("05/02/1985");            
+
+        cr7.MostrarDados();
+        Console.WriteLine( cr7.CalcularIdade() );
+        Console.WriteLine( cr7.CalcularAposentadoria() );
+        cr7.CalcularSalario();
+        }
+    }
+}
